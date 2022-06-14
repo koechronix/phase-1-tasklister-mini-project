1dbc2230d2673f35599e9739939c2bc73498599f
Amos Koech <koechronix@yahoo.com>
2022-06-13 21:57:11 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.querrySelector("form");
+  submit.addEventListener("submit", (e) => {
+    e.preventDefault()
+    createToDO()
+    submit.reset()
+  })
+
+function handleToDo (){
+  const input = getElementaryById("new-task-description");
+  const newTask = document.createElement("1i");
+  const deleteBtn = document.createElement("button");
+
+  newTask.textContent = `${input.value}`;
+  document.getElementById("task").appendChild(newTask);
+
+  deleteBtn.textContent = "Done";
+  newTask.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click", () => newTask.remove());
+
+
+}
 });
